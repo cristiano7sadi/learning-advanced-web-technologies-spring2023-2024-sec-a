5e4be6cd2ad4e3d40ae5668ac576643c6eb840bd
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-05-15 14:52:51 +0600

    Add files via upload

---
+++ trip-nest-backend-jami/src/payment/payment.module.ts
@@ -0,0 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { PaymentService } from './payment.service';
+// import { PaymentController } from './payment.controller';
+
+// @Module({
+//   controllers: [PaymentController],
+//   providers: [PaymentService],
+// })
+// export class PaymentModule {}
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module'; // Import UsersModule
+import { Payment } from './entities/payment.entity';
+import { PaymentService } from './payment.service';
+import { PaymentController } from './payment.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Payment]), UsersModule],
+  controllers: [PaymentController],
+  providers: [PaymentService],
+  exports: [PaymentService],
+})
+export class PaymentModule {}
